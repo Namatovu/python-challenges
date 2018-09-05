14643a73de99dbf72f6719695b182fc57964784c
Namatovu <namatovudamalie02@gmail.com>
2018-09-05 15:23:54 -0700

    refactores the code and adds comments

--- age.py
+++ age.py
@@ -4,11 +4,7 @@ currentDate=2018 # variable holding the current date to calculate age
 def age():
 	age=currentDate-int(Year_of_birth)  #formula to determine age
 	if age<18:
-		print("You are a minor.")
-		
-	if age>=18 and age<36:
-		print("You are a youth.")
-		
+		print("You are a mino
 	if age>=36:
 		print("You are an elder")
 		
