dcac7c855e0898a2ef547a991f9b878154102ade
Namatovu <namatovudamalie02@gmail.com>
2018-09-05 12:55:46 -0700

    refactores the code and adds comments

--- age.py
+++ age.py
@@ -1,12 +1,55 @@
-Year_of_birth=input("Which year were you born?")
-currentDate=2018
-age=currentDate-int(Year_of_birth)
-if age<18:
-    print("You are a minor.")
+Year_of_birth=input("Which year were you born?:") # prompting the user to enter the year they were born
+currentDate=2018 # variable holding the current date to calculate age
 
-if age>=18 and age<36:
-    print("You are a youth.")
+def age():
+	age=currentDate-int(Year_of_birth)  #formula to determine age
+	if age<18:
+		print("You are a minor.")
+		
+	if age>=18 and age<36:
+		print("You are a youth.")
+		
+	if age>=36:
+		print("You are an elder")
+		
+age()
+	
+    
 
 
-if age>=36: 
-    print("You are an elder")
+	
+    
+
+
+ 
+    
+	
+	
+
+
+
+
+
+
+
+
+
+	
+    
+
+
+	
+    
+
+
+ 
+    
+	
+	
+
+
+
+
+
+
+
